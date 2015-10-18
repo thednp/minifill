a440c1c37d012a37b062d8d9e43f47b6f77be0e7
thednp <thednp@users.noreply.github.com>
2015-10-18 08:12:53 +0300

    Create objectKeys.js

---
+++ objectKeys.js
@@ -0,0 +1,13 @@
+if (!Object.keys) {
+  Object.keys = function(obj) {
+    var keys = [];
+
+    for (var i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        keys.push(i);
+      }
+    }
+
+    return keys;
+  };
+}
