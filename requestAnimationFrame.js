77e8f5493403a34acadaf6333dbfc1ec385b9efe
thednp <thednp@users.noreply.github.com>
2016-03-07 14:33:49 +0200

    Create requestAnimationFrame.js

---
+++ requestAnimationFrame.js
@@ -0,0 +1,29 @@
+
+// requestAnimationFrame
+if (!window.requestAnimationFrame) {
+
+	var	lastTime = Date.now();
+	window.requestAnimationFrame = function (callback) {
+		'use strict';
+		if (typeof callback !== 'function') {
+			throw new TypeError(callback + 'is not a function');
+		}
+		
+		var	currentTime = Date.now(),
+			delay = 16 + lastTime - currentTime;
+
+		if (delay < 0) { delay = 0;	}
+
+		lastTime = currentTime;
+
+		return setTimeout(function () {
+			lastTime = Date.now();
+
+			callback(performance.now());
+		}, delay);
+	};
+
+	window.cancelAnimationFrame = function (id) {
+		clearTimeout(id);
+	};
+}
