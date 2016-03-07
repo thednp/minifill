7d4493d5288d3756a0dd2cbb9169be944d460f8c
thednp <thednp@users.noreply.github.com>
2016-03-07 14:34:13 +0200

    Update minifill.js

--- minifill.js
+++ minifill.js
@@ -162,36 +162,6 @@ if (!('getComputedStyle' in window)) {
 	})();
 }
 
-// requestAnimationFrame
-if (!window.requestAnimationFrame) {
-
-	var	lastTime = Date.now();
-	window.requestAnimationFrame = function (callback) {
-		'use strict';
-		if (typeof callback !== 'function') {
-			throw new TypeError(callback + 'is not a function');
-		}
-		
-		var	currentTime = Date.now(),
-			delay = 16 + lastTime - currentTime;
-
-		if (delay < 0) { delay = 0;	}
-
-		lastTime = currentTime;
-
-		return setTimeout(function () {
-			lastTime = Date.now();
-
-			callback(performance.now());
-		}, delay);
-	};
-
-	window.cancelAnimationFrame = function (id) {
-		clearTimeout(id);
-	};
-}
-
-
 // Event
 if (!window.Event||!Window.prototype.Event) {
 	(function (){
