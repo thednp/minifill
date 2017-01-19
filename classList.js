bed1e659fe416684493467ec0ad8a16ba6de6d7c
thednp <thednp@users.noreply.github.com>
2017-01-19 17:47:14 +0200

    Update classList.js

--- classList.js
+++ classList.js
@@ -1,69 +1,40 @@
-// Element.prototype.classList by Remy Sharp
-if (!("classList" in document.documentElement)) {
-	(function () {
-		var prototype = Array.prototype,
-			push = prototype.push,
-			splice = prototype.splice,
-			join = prototype.join;
-		
-		function ClassList(el) {
-			this.el = el;
-			// The className needs to be trimmed and split on whitespace
-			// to retrieve a list of classes.
-			var classes = el.className.replace(/^\s+|\s+$/g,'').split(/\s+/);
-			for (var i = 0; i < classes.length; i++) {
-				push.call(this, classes[i]);
-			}
-		};
-		
-		ClassList.prototype = {
-			add: function(token) {
-				if(this.contains(token)) return;
-				push.call(this, token);
-				this.el.className = this.toString();
-			},
-			contains: function(token) {
-				return this.el.className.indexOf(token) != -1;
-			},
-			item: function(index) {
-				return this[index] || null;
-			},
-			remove: function(token) {
-				if (!this.contains(token)) return;
-				for (var i = 0; i < this.length; i++) {
-					if (this[i] == token) break;
-				}
-				splice.call(this, i, 1);
-				this.el.className = this.toString();
-			},
-			toString: function() {
-				return join.call(this, ' ');
-			},
-			toggle: function(token) {
-				if (!this.contains(token)) {
-					this.add(token);
-				} else {
-					this.remove(token);
-				}
-			
-				return this.contains(token);
-			}
-		};
-		
-		window.classList = ClassList;
-		
-		function defineElementGetter (obj, prop, getter) {
-			if (Object.defineProperty) {
-				Object.defineProperty(obj, prop,{
-					get : getter
-				});
-			} else {
-				obj.__defineGetter__(prop, getter);
-			}
-		}
-		
-		defineElementGetter(Element.prototype, 'classList', function () {
-			return new ClassList(this);
-		});
-	})();
+// Element.prototype.classList polyfill
+// developed by thednp
+// license MIT
+
+if( !('classList' in Element.prototype) ) {
+  (function(){
+    var className = 'className', add = 'add', classList = 'classList', remove = 'remove', contains = 'contains',
+        prototype = 'prototype', element = 'element';
+
+    // classList definition
+    function ClassLIST(elem){
+      this[element] = elem;
+      this[classList] = elem[classList] = [];
+      var classesLIST = elem[className].replace(/^\s+|\s+$/g,'').split(/\s+/);
+      for (var i = 0; i < classesLIST.length; i++) {
+        this[classList].push(classesLIST[i]);
+      }
+    }
+
+    // methods
+    ClassLIST[prototype][add] = function(classNAME){
+      if (this[classList].indexOf(classNAME)<0) {
+        this[classList].push(classNAME);
+        this[element][className] = this[classList].join(' ');
+      }
+    };
+    ClassLIST[prototype][remove] = function(classNAME){
+      var classINDEX = this[classList].indexOf(classNAME);
+      if (classINDEX>-1) {
+        this[classList].splice(classINDEX,1);
+        this[element][className] = this[classList].join(' ');
+      }
+    };
+    ClassLIST[prototype][contains] = function(classNAME){
+      return this[classList].indexOf(classNAME) > -1;
+    };
+
+    Object.defineProperty(Element[prototype], classList, { get: function () { return new ClassLIST(this); } });
+  }());
 }
